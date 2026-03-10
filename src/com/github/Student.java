f5bd85ca2c05f2cf03ac20d3bae9609d06af1b98
Uppara Hemanth <upparahemanth3@email.com>
2026-03-10 10:48:12 +0530

    added methods

--- src/com/github/Student.java
+++ src/com/github/Student.java
@@ -4,4 +4,22 @@ public class Student {
 	private int student_id;
 	private String Student_name;
 	private String course;
+	public int getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+	public String getStudent_name() {
+		return Student_name;
+	}
+	public void setStudent_name(String student_name) {
+		Student_name = student_name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
 }
